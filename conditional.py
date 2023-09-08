56540fbdf12976f8702f669f60c813e543b6a98e
ganesh <kummara0304@gmail.com>
2023-09-08 15:39:56 +0530

    first

---
+++ conditional.py
@@ -0,0 +1,35 @@
+'''
+decision making statements
+
+if
+else
+elif
+nested if
+
+'''
+
+'''
+
+syntax:
+if condition:
+    statements
+else:
+    statements
+'''
+
+# if False:
+#     print('nenu if!!')
+# elif True:
+#     print('nenu elif!!')
+# else:
+#     print('nenu else!!')
+
+age = 16
+if age >18:
+    print('you can vote')
+elif age == 18:
+    print('you can vote buddy')
+else:
+    print('wait')
+
+
---
+++ datatypes.py
@@ -0,0 +1,26 @@
+'''
+int (1,-1)
+boolean
+float
+complex
+
+'''
+
+a = 11
+b = -34
+print(type(a), type(b))
+
+c = True
+d = False
+print(type(c), type(d))
+
+e = 5.89
+print(type(e))
+
+v = 5+10j
+print(type(v))
+
+'''
+data lose --> explicit type conversion
+no data lose --> implicit type conversion
+'''
